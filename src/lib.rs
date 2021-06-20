49dfa2e5d7a609c2de60f893b6ab24d9dbc3b22b
Ruihang Xia <waynestxia@gmail.com>
2021-06-20 11:45:35 +0800

    *: use jemalloc
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -67,3 +67,6 @@ pub use fn_registry::FnRegistry;
 pub use level::{SimpleTimestampReviewer, TimestampAction, TimestampReviewer};
 pub use types::{Entry, TimeRange};
 pub use util::{Comparator, LexicalComparator, NoOrderComparator};
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
