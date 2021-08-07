c66dd70b8e1b5da2b9e8689d87ffb14fe618ddd4
Ruihang Xia <waynestxia@gmail.com>
2021-08-07 17:22:27 +0800

    file: remove deprecated
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,9 +1,7 @@
 mod file_manager;
-mod others;
 mod rick;
 mod sstable;
 
 pub use file_manager::{FileManager, FileType};
-pub use others::LevelInfoHandle;
 pub use rick::Rick;
 pub use sstable::{IndexBlockBuilder, SSTable, TableBuilder};
